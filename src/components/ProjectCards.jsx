4fe2ecf6c3a77fde2053d12d86686aa7c35ceac9
saoodbaloch <118266529+saoodbaloch@users.noreply.github.com>
2026-04-04 17:33:41 +0500

    Update ProjectCards.jsx

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -22,7 +22,7 @@ const projects = [
     title: "E-Book Store Website",
     category: "Online Bookstore",
     image: ebook4,
-    link: "https://ebook2.lovestoblog.com/",
+    link: "https://e-book.iceiy.com/",
   },
 ];
 
